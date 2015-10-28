7681b3a215262ff299d12f7593b5b433a95193c7
margaretyjkim <margaret.yj.kim@gmail.com>
2015-10-28 19:16:57 -0400

    everything but the bonus

--- exercise7.rb
+++ exercise7.rb
@@ -2,6 +2,8 @@
 def cohort (list)
 	list.each do |name, number|
 		puts "#{name}: #{number} students"
+		#number 4
+		puts (number * 1.05).to_i
 	end
 end
 
@@ -13,11 +15,30 @@ students = {
 
 
 #number 2
-students[:cohort4] = 42
+students[:cohort4] = 43
 list = cohort(students)
 puts list
+
 #number 3
 puts students.keys
 
 #number 4
+
 #number 5
+puts "*****delete 2******"
+students.delete:cohort2
+puts "*****delete 2******"
+puts "new list"
+list = cohort(students)
+
+puts list
+
+#BONUS
+
+# def total_students(sum)
+# 	sum.each do |name, number|
+# 		puts number
+# 	end
+# end
+# total = total_students(students.values)
+# puts total
