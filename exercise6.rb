ae81265ab99fa225c7d1c4e784cf7a64cd9b690e
margaretyjkim <margaret.yj.kim@gmail.com>
2015-10-28 17:22:16 -0400

    exercise 6 up to number 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,31 @@
+def added_list(grocery_list, new_item)
+	grocery_list << new_item
+end
+
+def look_for_items(grocery_list, search_item)
+	if grocery_list.include?(search_item)
+		puts "You don't need to pick up #{search_item} today."
+	else
+		puts "You need to pick up #{search_item} today."
+	end
+end
+
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list.each do |item|
-	puts "*#{item}"
+added_list(grocery_list, "rice")
+
+grocery_list.each_with_index do |item, index|
+	grocery_list[index] = "*#{item}"
 end
+
+
+puts grocery_list.sort
+puts "There are #{grocery_list.count} items that need to be picked up."
+
+
+puts "What are you looking for?"
+looking_for = gets.chomp
+
+look_for_items(grocery_list, looking_for)
+
+puts grocery_list[1]
